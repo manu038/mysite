722f101b72c8b7cd9dfceb9e4e80447083c07166
vishvendra.s <vishvendrasingh1292@gmail.com>
2018-10-22 03:01:26 +0530

    my second commit

--- manage.py
+++ manage.py
@@ -19,4 +19,5 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
+#this is my first comment
     execute_from_command_line(sys.argv)
